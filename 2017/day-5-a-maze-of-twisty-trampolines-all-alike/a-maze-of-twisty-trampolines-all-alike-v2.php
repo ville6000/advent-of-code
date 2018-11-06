403ed61784cb05931c59443dd9f81eed7a5045a9
Ville Viklund <ville.viklund@morgan.fi>
2018-11-06 10:07:57 +0200

    Add day 5

---
+++ 2017/day-5-a-maze-of-twisty-trampolines-all-alike/a-maze-of-twisty-trampolines-all-alike-v2.php
@@ -0,0 +1,33 @@
+<?php
+
+class MazeSolver
+{
+    public function formatInput()
+    {
+        return explode("\n", file_get_contents('./input'));
+    }
+
+    public function solve()
+    {
+        $steps = $this->formatInput($input);
+        return $this->doSolveMaze($steps);
+    }
+
+    public function doSolveMaze($steps) 
+    {
+        $stepCount = 0;
+        $currentIndex = 0;
+
+        while ($currentIndex < count($steps)) {
+            $stepCount++;
+            $newIndex = $currentIndex + $steps[$currentIndex];
+            $steps[$currentIndex] = $steps[$currentIndex] >= 3 ? --$steps[$currentIndex] : ++$steps[$currentIndex];
+            $currentIndex = $newIndex;
+        }
+
+        return $stepCount;
+    }
+}
+
+$mazeSolver = new MazeSolver();
+echo $mazeSolver->solve();
---
+++ 2017/day-5-a-maze-of-twisty-trampolines-all-alike/a-maze-of-twisty-trampolines-all-alike.php
@@ -0,0 +1,34 @@
+<?php
+
+class MazeSolver
+{
+    public function formatInput()
+    {
+        return explode("\n", file_get_contents('./input'));
+    }
+
+    public function solve()
+    {
+        $steps = $this->formatInput($input);
+        return $this->doSolveMaze($steps);
+    }
+
+    public function doSolveMaze($steps) 
+    {
+        $stepCount = 0;
+        $currentIndex = 0;
+
+        while ($currentIndex < count($steps)) {
+            $stepCount++;
+            echo "Current index: $currentIndex point value: $steps[$currentIndex] \n";
+            $newIndex = $currentIndex + $steps[$currentIndex];
+            $steps[$currentIndex]++;
+            $currentIndex = $newIndex;
+        }
+
+        return $stepCount;
+    }
+}
+
+$mazeSolver = new MazeSolver();
+echo $mazeSolver->solve();
