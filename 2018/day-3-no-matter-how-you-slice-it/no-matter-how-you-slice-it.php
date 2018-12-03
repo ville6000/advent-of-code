41be154547d28ac788c2412dd135b8740143c5f3
Ville Viklund <ville.viklund@morgan.fi>
2018-12-03 10:52:09 +0200

    Add day 3 part 1 & 2

---
+++ 2018/day-3-no-matter-how-you-slice-it/no-matter-how-you-slice-it.php
@@ -0,0 +1,88 @@
+<?php
+
+function overlapCount($diagrams)
+{
+    $diagrams = array_map('normalizeDiagram', $diagrams);
+    $fabric = [];
+    $overlapCount = 0;
+
+    foreach ($diagrams as $diagram) {
+        $fabric = fillFabric($diagram, $fabric);
+    }
+
+    foreach ($fabric as $row) {
+        foreach ($row as $cell) {
+            if (count($cell) > 1) {
+                $overlapCount++;
+            }
+        }
+    }
+
+    return $overlapCount;
+}
+
+function doesNotOverlap($diagrams)
+{
+    $diagrams = array_map('normalizeDiagram', $diagrams);
+    $fabric = [];
+    $overlapCount = 0;
+
+    $idList = [];
+    foreach ($diagrams as $diagram) {
+        $idList[$diagram['id']] = $diagram['id'];
+        $fabric = fillFabric($diagram, $fabric);
+    }
+
+    foreach ($fabric as $row) {
+        foreach ($row as $cell) {
+            if (count($cell) > 1) {
+                $overlapCount++;
+
+                foreach ($cell as $item) {
+                    unset($idList[$item]);
+                }
+            }
+        }
+    }
+
+    return implode('', $idList);
+}
+
+function normalizeDiagram($diagram)
+{
+    $parts = explode(' ', $diagram);
+    $coordinates = explode(',', $parts[2]);
+    $area = explode('x', $parts[3]);
+
+    return [
+        'id' => substr($parts[0], 1),
+        'left' => $coordinates[0],
+        'top' => substr($coordinates[1], 0, -1),
+        'width' => $area[0],
+        'height' => $area[1],
+    ];
+}
+
+function fillFabric($diagram, $fabric)
+{
+    $rightLimit = $diagram['left'] + $diagram['width'];
+    $bottomLimit = $diagram['top'] + $diagram['height'];
+
+    for ($rowIdx = $diagram['top']; $rowIdx < $bottomLimit; $rowIdx++) {
+        for ($colIdx = $diagram['left']; $colIdx < $rightLimit; $colIdx++) {
+            if (!isset($fabric[$rowIdx][$colIdx])) {
+                $fabric[$rowIdx][$colIdx] = [];
+            }
+
+            $isHorizontalMatch = $colIdx >= $diagram['left'] && $colIdx <= $rightLimit;
+            $isVerticalMatch = $rowIdx >= $diagram['top'] && $rowIdx <= $bottomLimit;
+
+            if ($isHorizontalMatch && $isVerticalMatch) {
+                $fabric[$rowIdx][$colIdx][] = $diagram['id'];
+            }
+        }
+    }
+
+    return $fabric;
+}
+
