62d0752790f6c0831c6e93e1b667b34496ccf67d
Ville Viklund <ville.viklund@morgan.fi>
2018-12-05 23:23:17 +0200

    Day 5 part 2

--- 2018/day-5-alchemical-reduction/alchemical-reduction.js
+++ 2018/day-5-alchemical-reduction/alchemical-reduction.js
@@ -34,11 +34,29 @@ const doReact = (parts, i) => {
     return false;
 };
 
-const part1 = () => {
-    const fs = require('fs');
+const findUniqueReactingUnits = (parts) => {
+    const unique = [];
 
-    const contents = fs.readFileSync('./input.txt', 'utf8');
-    let parts = contents.split('');
+    for (let i = 0; i < parts.length; i++) {
+        let next = parts[i + 1];
+        if (typeof next !== 'undefined') {
+            let a = parts[i];
+            let b = parts[i + 1];
+
+            if (isReactivePair(a, b)) {
+                if (typeof next !== 'undefined') {
+                    parts.splice(i, 2);
+                    unique.push(a.toLowerCase());
+                }
+            }
+        }
+    }
+
+    return Array.from(new Set(unique));
+};
+
+const findLength = (string) => {
+    let parts = string.split('');
     let nextKey = 0;
 
     while (nextKey !== false) {
@@ -47,7 +65,34 @@ const part1 = () => {
     }
 
     return parts.length;
+};
 
+const part1 = () => {
+    const fs = require('fs');
+    const contents = fs.readFileSync('./input.txt', 'utf8');
+
+    return findLength(contents);
+};
+
+const part2 = () => {
+    const fs = require('fs');
+    const contents = fs.readFileSync('./input.txt', 'utf8');
+    const unique = findUniqueReactingUnits(contents.split(''));
+    let shortest = false;
+
+    unique.forEach(letter => {
+        const string = contents.replace(new RegExp(letter, 'gi'), '');
+        const length = findLength(string);
+
+        if (shortest === false) {
+            shortest = length;
+        } else if (shortest > length) {
+            shortest = length;
+        }
+    });
+
+    return shortest;
 };
 
 console.log("Part 1: ", part1());
+console.log("Part 2: ", part2());
