45ab247b9b25e81895343970ebeeddb29332c3c8
Ville Viklund <ville.viklund@morgan.fi>
2018-12-11 16:42:43 +0200

    Add timing

--- 2018/day-11-chronal-charge/chronal-charge.php
+++ 2018/day-11-chronal-charge/chronal-charge.php
@@ -8,10 +8,8 @@ function calculatePower($yCoordinate, $xCoordinate, $serial)
     return ($power < 100) ? 0 : substr(strrev($power), 2, 1) - 5;
 }
 
-function part1()
+function createPowerGrid($serial, $gridLimit)
 {
-    $serial = 9424;
-    $gridLimit = 300;
     $grid = [];
 
     for ($i = 1; $i <= $gridLimit; $i++) {
@@ -20,22 +18,37 @@ function part1()
         }
     }
 
+    return $grid;
+}
+
+function calculateGridPower($grid, $row, $column, $size)
+{
+    $totalPower = 0;
+    for ($i = $row; $i < $row + $size; $i++) {
+        $currentRow = $grid[$i];
+        $totalPower += array_sum(array_slice($currentRow, ($column - 1), $size));
+    }
+
+    return $totalPower;
+}
+
+function part1()
+{
+    $serial = 9424;
+    $gridLimit = 300;
+
+    $grid = createPowerGrid($serial, $gridLimit);
+
     $row = 1;
     $step = 3;
     $largestTotalPower = 0;
     $topLeft = false;
 
-    while ($row <= $gridLimit) {
+    while ($row <= ($gridLimit - $step)) {
         $column = 1;
 
-        while ($column <= $gridLimit) {
-            $totalPower = 0;
-
-            for ($i = $row; $i < $row + $step; $i++) {
-                for ($j = $column; $j < $column + $step; $j++) {
-                    $totalPower += $grid[$i][$j];
-                }
-            }
+        while ($column <= ($gridLimit - $step)) {
+            $totalPower = calculateGridPower($grid, $row, $column, $step);
 
             if ($totalPower > $largestTotalPower) {
                 $largestTotalPower = $totalPower;
@@ -51,4 +64,47 @@ function part1()
     return $topLeft;
 }
 
+function part2()
+{
+    $serial = 9424;
+    $gridLimit = 300;
+
+    $grid = createPowerGrid($serial, $gridLimit);
+
+    $row = 1;
+    $largestTotalPower = 0;
+    $topLeftSize = false;
+
+    while ($row <= $gridLimit) {
+        $column = 1;
+
+        while ($column <= $gridLimit) {
+            $maxSize = ($row > $column) ? $gridLimit - $row : $gridLimit - $column;
+
+            for ($size = 1; $size <= $maxSize; $size++) {
+                $totalPower = calculateGridPower($grid, $row, $column, $size);
+
+                if ($totalPower > $largestTotalPower) {
+                    $largestTotalPower = $totalPower;
+                    $topLeftSize = "$column,$row,$size";
+                }
+            }
+
+            $column++;
+        }
+
+        $row++;
+    }
+
+    return $topLeftSize;
+}
+
+$totalTime = microtime(true);
+$part1Time = microtime(true);
 echo "Part 1: " . part1() . "\n";
+echo "Part 1 duration: " . (microtime(true) - $part1Time) ."\n";
+
+$part2Time = microtime(true);
+echo "Part 2: " . part2() . "\n";
+echo "Part 2 duration: " . (microtime(true) - $part2Time) ."\n";
+echo "Total duration: " . (microtime(true) - $totalTime) ."\n";
