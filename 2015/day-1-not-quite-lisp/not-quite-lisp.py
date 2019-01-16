bba86d74d1f77dc0a3a4ed23cc71ba2b2fa8bfd0
Ville Viklund <ville.viklund@morgan.fi>
2019-01-16 11:27:42 +0200

    Less verbose

--- 2015/day-1-not-quite-lisp/not-quite-lisp.py
+++ 2015/day-1-not-quite-lisp/not-quite-lisp.py
@@ -3,23 +3,17 @@ def part1():
     floor = 0
 
     for instruction in instructions:
-        if instruction == '(':
-            floor += 1
-        else:
-            floor -= 1
+        floor = floor + 1 if instruction == '(' else floor - 1
 
     return floor
 
+
 def part2():
     instructions = list(open('./input.txt', 'r').readline())
-    floor = 0
-    idx = 0
+    floor = idx = 0
 
     for instruction in instructions:
-        if instruction == '(':
-            floor += 1
-        else:
-            floor -= 1
+        floor = floor + 1 if instruction == '(' else floor - 1
 
         idx += 1
 
@@ -27,6 +21,7 @@ def part2():
             return idx
 
     return idx
+
 
 print("Part 1:", part1())
 print("Part 2:", part2())
