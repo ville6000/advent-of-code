b72427b703a79041a7e5aa5cf2f90dc1dd0a2838
Ville Viklund <ville.viklund@morgan.fi>
2019-12-07 13:33:13 +0200

    Day 1 part 2

--- 2019/day-1/day1.js
+++ 2019/day-1/day1.js
@@ -11,3 +11,35 @@ const part1 = () => {
 };
 
 console.log(part1());
+
+const part2 = () => {
+  const fs = require('fs');
+  const lines = fs
+    .readFileSync('./input.txt', 'utf8')
+    .trim()
+    .split('\n');
+
+  return lines
+    .map(mass => getTotalFuel(mass))
+    .reduce((total, current) => total + current, 0);
+};
+
+const getTotalFuel = mass => {
+  let isPositive = true;
+  let total = 0;
+
+  while (isPositive) {
+    const fuel = Math.floor(mass / 3) - 2;
+
+    if (fuel > 0) {
+      total += fuel;
+      mass = fuel;
+    } else {
+      isPositive = false;
+    }
+  }
+
+  return total;
+};
+
+console.log(part2());
