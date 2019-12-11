05e81c68c5c57714e925b708e108986beb29cb8d
Ville Viklund <ville.viklund@morgan.fi>
2019-12-11 17:05:36 +0200

    Day 4 part 2

--- 2019/day-4/day4.js
+++ 2019/day-4/day4.js
@@ -29,4 +29,44 @@ const isValidPassword = password => {
   return true;
 };
 
-console.log(part1());
+console.log('Part 1:', part1());
+
+const part2 = () => {
+  const START = 168630;
+  const END = 718098;
+  let validCount = 0;
+
+  for (let i = START; i <= END; i++) {
+    if (isMostValidPassword(i)) {
+      validCount++;
+    }
+  }
+
+  return validCount;
+};
+
+const isMostValidPassword = password => {
+  const digits = Array.from(password.toString()).map(Number);
+
+  let repeatCounts = {};
+
+  for (let i = 0; i < digits.length; i++) {
+    if (i > 0 && digits[i] < digits[i - 1]) {
+      return false;
+    }
+
+    if (typeof repeatCounts[digits[i]] === 'undefined') {
+      repeatCounts[digits[i]] = 1;
+    } else {
+      repeatCounts[digits[i]]++;
+    }
+  }
+
+  const validRepeat = Object.keys(repeatCounts).filter(key => {
+    return repeatCounts[key] === 2;
+  });
+
+  return validRepeat.length >= 1;
+};
+
+console.log('Part 2:', part2());
