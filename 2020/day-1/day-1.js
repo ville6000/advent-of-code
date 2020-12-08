1b8a8df12d6add26776c85314eabe02469e03eb9
Ville Viklund <ville.viklund@geniem.com>
2020-12-08 15:31:53 +0200

    Vähemmän koodia ja prettierrc

--- 2020/day-1/day-1.js
+++ 2020/day-1/day-1.js
@@ -1,54 +1,42 @@
-const fs = require('fs');
+const fs = require("fs");
 const values = fs
-    .readFileSync('../inputs/day-1.txt', 'utf8')
-    .split("\n")
-    .map(item => parseInt(item));
+  .readFileSync("../inputs/day-1.txt", "utf8")
+  .split("\n")
+  .map((item) => parseInt(item));
 
 const part1 = (values) => {
-    const TARGET = 2020;
-    let found = false;
-    let currentIdx = 0;
-    
-    while (!found) {
-        for (let idx = 0; idx < values.length; idx++) {
-            if (idx !== currentIdx) {
-                let sum = values[idx] + values[currentIdx];
-    
-                if (sum === TARGET) {
-                    return values[idx] * values[currentIdx];
-                }
-            }
-        }
-    
-        currentIdx++;
-    }    
+  const TARGET = 2020;
 
-    return false;
-}
+  for (let i = 0; i < values.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      if (i !== j && TARGET === values[i] + values[j]) {
+        return values[i] * values[j];
+      }
+    }
+  }
+
+  return false;
+};
 
 const part2 = (values) => {
-    const TARGET = 2020;
-    let found = false;
-    let currentIdx = 0;
-    
-    while (!found) {
-        for (let i = 0; i < values.length; i++) {
-            for (let j = 0; j < values.length; j++) {
-                if (i !== currentIdx && j !== currentIdx && i !== j) {
-                    let sum = values[i] + values[j] + values[currentIdx];
-        
-                    if (sum === TARGET) {
-                        return values[i] *values[j] * values[currentIdx];
-                    }
-                }
-            }
-        }
-    
-        currentIdx++;
-    }    
+  const TARGET = 2020;
 
-    return false;
-}
+  for (let i = 0; i < values.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      for (let k = 0; k < values.length; k++) {
+        if (i !== k && j !== k && i !== j) {
+          let sum = values[i] + values[j] + values[k];
+
+          if (sum === TARGET) {
+            return values[i] * values[j] * values[k];
+          }
+        }
+      }
+    }
+  }
+
+  return false;
+};
 
 console.time();
 console.log("Part 1", part1(values));
