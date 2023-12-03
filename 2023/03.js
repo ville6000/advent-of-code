93c7424562d4e2b209410908e498793780cb6aa3
Ville Viklund <ville.viklund@geniem.com>
2023-12-03 22:45:50 +0200

    Add day 3 part 2

--- 2023/03.js
+++ 2023/03.js
@@ -18,6 +18,55 @@ const part1 = () => {
   return sum;
 };
 
+const part2 = () => {
+  const numbers = findNumbers(input);
+  const gears = findGears(input, numbers);
+  let total = 0;
+
+  for (const gear of gears) {
+    const found = numbers.filter((number) => inRange(number, gear));
+
+    if (found.length > 1) {
+      total += eval(found.map((n) => n.num).join("*"));
+    }
+  }
+
+  return total;
+};
+
+const inRange = (number, gear) => {
+  const rows = [gear.row - 1, gear.row, gear.row + 1];
+
+  if (!rows.includes(number.row)) {
+    return false;
+  }
+
+  const min = number.col > 0 ? number.col - 1 : number.col;
+  const max = number.col + number.length;
+
+  return gear.col >= min && gear.col <= max;
+};
+
+const findGears = (input) => {
+  let row = 0;
+  const gears = [];
+
+  for (const line of input) {
+    const regex = /\*/g;
+    let match = [...line.matchAll(regex)];
+
+    if (match !== null) {
+      match.forEach((gear) => {
+        gears.push({ row, col: gear.index });
+      });
+    }
+
+    row++;
+  }
+
+  return gears;
+};
+
 const hasAdjecentSymbol = (grid, number) => {
   const rows = [number.row - 1, number.row, number.row + 1];
 
@@ -34,15 +83,11 @@ const hasAdjecentSymbol = (grid, number) => {
   }
 
   return false;
-}
+};
 
 const isSymbol = (char) => {
-  if (char === '.' || !isNaN(parseInt(char))) {
-    return false;
-  }
-
-  return true;
-}
+  return char !== "." && Number.isNaN(parseInt(char));
+};
 
 const findNumbers = (input) => {
   const numbers = [];
@@ -67,8 +112,12 @@ const findNumbers = (input) => {
   }
 
   return numbers;
-}
+};
 
 console.time("part1");
 console.log(part1());
 console.timeEnd("part1");
+
+console.time("part2");
+console.log(part2());
+console.timeEnd("part2");
