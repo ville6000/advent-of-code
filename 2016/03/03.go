012734cd2fe745937b17a071593fa875da2ad491
Ville Viklund <ville.viklund@geniem.com>
2023-12-10 00:02:12 +0200

    Add 2016 day 03

--- 2016/03/03.go
+++ 2016/03/03.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	input := readInput()
 	fmt.Println("Part 1:", part1(input))
+	fmt.Println("Part 2:", part2(input))
 }
 
 func part1(lines []string) int {
@@ -18,10 +19,32 @@ func part1(lines []string) int {
 
 	for _, line := range lines {
 		a, b, c := getSides(line)
-		fmt.Println(a, b, c)
 
 		if a+b > c && a+c > b && b+c > a {
 			total++
+		}
+	}
+
+	return total
+}
+
+func part2(lines []string) int {
+	colTreshold := 3
+	total := 0
+
+	for col := 0; col < colTreshold; col++ {
+		for i := 0; i < len(lines); i += 3 {
+			parts1 := strings.Fields(lines[i])
+			parts2 := strings.Fields(lines[i+1])
+			parts3 := strings.Fields(lines[i+2])
+
+			a, _ := strconv.Atoi(parts1[col])
+			b, _ := strconv.Atoi(parts2[col])
+			c, _ := strconv.Atoi(parts3[col])
+
+			if a+b > c && a+c > b && b+c > a {
+				total++
+			}
 		}
 	}
 
