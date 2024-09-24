fb7b400a927e0ef3e7c48428bef887031709bf1a
Ville Viklund <ville.viklund@svea.fi>
2024-09-24 21:54:08 +0300

    Day 02 part 1

---
+++ 2016/02/02.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := getInput()
+
+	part1(input)
+}
+
+func part1(input []string) {
+	start := 5
+	var code []int
+
+	for _, line := range input {
+		start = findCode(start, strings.Split(line, ""))
+		code = append(code, start)
+	}
+
+	fmt.Print(IntToString1(code) + "\n")
+}
+
+func findCode(start int, moves []string) int {
+	pos := start
+	step := 3
+
+	for _, direction := range moves {
+		switch {
+		case direction == "U":
+			if pos-step > 0 {
+				pos = pos - step
+			}
+		case direction == "L":
+			if pos%step != 1 {
+				pos--
+			}
+		case direction == "R":
+			if pos%step != 0 {
+				pos++
+			}
+		case direction == "D":
+			if pos+step <= step*step {
+				pos = pos + step
+			}
+		}
+	}
+
+	return pos
+}
+
+func getInput() []string {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
+
+func IntToString1(a []int) string {
+	b := ""
+	for _, v := range a {
+		if len(b) > 0 {
+			b += ""
+		}
+		b += strconv.Itoa(v)
+	}
+
+	return b
+}
